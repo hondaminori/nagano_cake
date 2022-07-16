1794b1d434b17f92ef81010869d6c599a71fc081
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-07-17 03:01:35 +0900

    [3.1.3 Finish]ジャンル編集画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -10,6 +10,16 @@ class Admin::GenresController < ApplicationController
     redirect_to admin_genres_path
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
+  end
+
   def genre_params
     params.require(:genre).permit(:name)
   end
