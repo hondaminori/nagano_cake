67c27a8f847a33b94c8f5388b11cfb7b6351beac
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-07-17 04:06:20 +0900

    [3.1.4 Update]商品新規登録画面 model作成とseeds.rb追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_12_224245) do
+ActiveRecord::Schema.define(version: 2022_07_16_183523) do
 
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 2022_07_12_224245) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,29 @@
 # )
 
 # ジャンル
-Genre.create!(
-  name: "ケーキ"
-)
+# Genre.create!(
+#   name: "ケーキ"
+# )
 
-Genre.create!(
-  name: "プリン"
-)
+# Genre.create!(
+#   name: "プリン"
+# )
 
-Genre.create!(
-  name: "焼き菓子"
-)
+# Genre.create!(
+#   name: "焼き菓子"
+# )
 
-Genre.create!(
-  name: "キャンディ"
-)
+# Genre.create!(
+#   name: "キャンディ"
+# )
+
+# 商品 ※もちろんベタ書きでもOK
+5.times do |n|
+  Item.create!(
+    genre_id: 0,
+    name: "商品#{n + 1}",
+    introduction: "商品#{n + 1}の説明商品#{n + 1}の説明商品#{n + 1}の説明",
+    price: 0,
+    is_active: true
+  )
+end
