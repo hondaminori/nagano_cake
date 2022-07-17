f1f6b0b355b1281f55f49622e3088dc65890c6fb
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-07-18 08:11:11 +0900

    [3.1.6 Finish]商品詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,9 +7,17 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def create
     item = Item.new(item_params)
     item.save
+  end
+
+  def edit
+    @item = Item.find(params[:id])
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  def tax_include
+    (price * 1.1).floor
+  end
 end
