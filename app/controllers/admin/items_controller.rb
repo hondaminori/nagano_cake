c57bacd0f8e4ed9aef59403f8747dd4e6fe193d3
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-07-18 09:16:18 +0900

    [3.1.7 Finish]商品編集画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,15 +14,22 @@ class Admin::ItemsController < ApplicationController
   def create
     item = Item.new(item_params)
     item.save
+    redirect_to admin_item_path(item)
   end
 
   def edit
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item)
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price)
+    params.require(:item).permit(:name, :introduction, :price, :image)
   end
 end
