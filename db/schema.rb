98a6aa38e91e33d202271a1fd2ed3a4aa35ce4c7
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-07-18 10:44:11 +0900

    [3.1.8 会員一覧画面]Start

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_231135) do
+ActiveRecord::Schema.define(version: 2022_07_18_011540) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,14 @@ ActiveRecord::Schema.define(version: 2022_07_16_231135) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
+    t.string "first_name"
+    t.string "last_name_kana"
+    t.string "first_name_kana"
+    t.string "postal_code"
+    t.string "address"
+    t.string "telephone_number"
+    t.boolean "is_active", default: true
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,12 +30,28 @@
 # )
 
 # 商品 ※もちろんベタ書きでもOK
+# 5.times do |n|
+#   Item.create!(
+#     genre_id: 0,
+#     name: "商品#{n + 1}",
+#     introduction: "商品#{n + 1}の説明商品#{n + 1}の説明商品#{n + 1}の説明",
+#     price: 100 * n,
+#     is_active: true
+#   )
+# end
+
+# 顧客 nが10以上なら、電話番号の桁数が増えますので注意。どうでもいいけど。
 5.times do |n|
-  Item.create!(
-    genre_id: 0,
-    name: "商品#{n + 1}",
-    introduction: "商品#{n + 1}の説明商品#{n + 1}の説明商品#{n + 1}の説明",
-    price: 100 * n,
+  Customer.create!(
+    last_name: "姓姓姓#{n+1}",
+    first_name: "名名名#{n+1}",
+    last_name_kana: "セイセイセイ#{n+1}",
+    first_name_kana: "メイメイメイ#{n+1}",
+    email: "sei#{n+1}@testdata.com",
+    password: "passw0rd",
+    postal_code: "#{1500041 + n}",
+    address: "東京都渋谷区神南1丁目19-1#{n + 1} パークウェースクエア2 #{n + 4}階",
+    telephone_number: "036869470#{n + 1}",
     is_active: true
   )
 end
