01c607e97a2b65b7435046e2db8da4fff6bfee42
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-07-19 20:48:08 +0900

    [3.1.11 Start]注文履歴一覧画面

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_18_011540) do
+ActiveRecord::Schema.define(version: 2022_07_18_215802) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -87,6 +87,19 @@ ActiveRecord::Schema.define(version: 2022_07_18_011540) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["genre_id"], name: "index_items_on_genre_id"
+  end
+
+  create_table "orders", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "shipping_cost"
+    t.integer "total_payment"
+    t.integer "payment_method"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -43,18 +43,41 @@
 # 顧客 nが10以上なら、電話番号の桁数が増えますので注意。どうでもいいけど。
 # また、5件に1件くらいは退会済みがあってもいいかなと思って。
 # 全件消すときは、rails cでコンソールに入ってCustomer.delete_allでどうぞ。
-5.times do |n|
-  Customer.create!(
-    last_name: "姓姓#{n}",
-    first_name: "名名#{n}",
-    last_name_kana: "セイセイセイ#{n}",
-    first_name_kana: "メイメイメイ#{n}",
-    email: "sei#{n}@testdata.com",
-    password: "passw0rd",
-    postal_code: "#{1500041 + n}",
-    address: "東京都渋谷区神南1丁目19-#{n} 
-      パークウェースクエア2 #{n + 1}階",
-    telephone_number: "036869470#{n}",
-    is_active: (n % 5 == 0) ? false : true
-  )
-end
+# 5.times do |n|
+#   Customer.create!(
+#     last_name: "姓姓#{n}",
+#     first_name: "名名#{n}",
+#     last_name_kana: "セイセイセイ#{n}",
+#     first_name_kana: "メイメイメイ#{n}",
+#     email: "sei#{n}@testdata.com",
+#     password: "passw0rd",
+#     postal_code: "#{1500041 + n}",
+#     address: "東京都渋谷区神南1丁目19-#{n} 
+#       パークウェースクエア2 #{n + 1}階",
+#     telephone_number: "036869470#{n}",
+#     is_active: (n % 5 == 0) ? false : true
+#   )
+# end
+
+# 注文履歴
+# 会員ID（CustomerのID）が必要になるので、頑張れば何とか自動処理が
+# 作れたのかもしれないが、手でセットするようにした。その他情報も手で
+# セットしたIDから取得した。
+
+# 手でセットするものたち
+tmp_customer = Customer.find(44)  # findのカッコの中
+tmp_total_payment = 10000 # 注文履歴詳細の合計額を手入力
+tmp_payment_method = 0 # 0: クレジットカード, 1: 銀行振込
+tmp_status = 0 # 0: 入金待ち, 1:入金確認, 2: 製作中, 3: 発送準備中, 4: 発送済み
+# puts '会員ID: ' + tmp_customer.id.to_s
+
+Order.create!(
+  customer_id: tmp_customer.id,
+  postal_code: tmp_customer.postal_code,
+  address: tmp_customer.address,
+  name: tmp_customer.last_name + tmp_customer.first_name,
+  shipping_cost: 800, # 固定値（仕様より）
+  total_payment: tmp_total_payment + 800,
+  payment_method: tmp_payment_method,
+  status: tmp_status
+)
