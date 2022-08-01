5474047d46284b4577966e0d96984dd4c05b372d
hondaminori.ta3 <hondaminori.ta3@gmail.com>
2022-08-02 07:11:06 +0900

    [3.2.2 Finish]About

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,7 @@ class HomesController < ApplicationController
   def top
     @items = Item.order('created_at DESC').limit(4)
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "homes#top"
+  get 'about' => 'homes#about', as: 'about'
 
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
